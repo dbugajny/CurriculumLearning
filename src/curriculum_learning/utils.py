24e50ae31c5f790c35c58ba5ec70996cd0f69616
dbugajny <dawid.bugajny@gmail.com>
2024-02-18 12:50:27 +0100

    commit

--- src/curriculum_learning/utils.py
+++ src/curriculum_learning/utils.py
@@ -11,7 +11,14 @@ def unpickle(filepath):
 def load_data(filepath):
     data = unpickle(filepath)
 
-    x = data[b"data"].reshape(10000, 3, 32, 32).transpose(0, 3, 2, 1).astype("float32")
+    x = data[b"data"].reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("float32")
     y = np.array(data[b"labels"])
 
     return x, y
+
+
+def calculate_loss_per_sample(y_true, y_pred, loss):
+    losses = []
+    for i in range(len(y_true)):
+        losses.append(loss(y_true[i], y_pred[i]))
+    return losses
